53fd6f6563e74333852c82edb65630fe86c90aae
theobori <theo1.bori@epitech.eu>
2022-01-16 03:36:00 +0100

    Added color render

--- cogs/renderer.py
+++ cogs/renderer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from turtle import color, title
 from typing import *
 
 import discord, json, uuid, os
@@ -17,7 +16,6 @@ config.read("config.ini")
 
 class Renderer(commands.Cog, Api):
     """Manage role add and update"""
-
 
     def __init__(self):
         Api().__init__()
@@ -41,23 +39,30 @@ class Renderer(commands.Cog, Api):
             f.write(content)
         return (filename)
 
-    async def _send_img(self, message: discord.Message, name: str, filename: str):
-        embed = discord.Embed(title=name, color=0x000000)
-
+    async def _send_img(self, message: discord.Message, name: str, filename: str, url: str=None):
+        embed_kwargs = {
+            "title": name, 
+            "color": 0x000000
+        }
+        if (url):
+            embed_kwargs["url"] = url
+        embed = discord.Embed(**embed_kwargs)
+            
         file = discord.File(filename, filename=filename)
         embed.set_image(url=f'attachment://{filename}')
         await message.channel.send(embed=embed, file=file)
         os.remove(filename)
 
-    async def _send_render(self, message: discord.message, url: str, name: str=None):
-        res = self.render({"skin": url})
+    async def _send_render(self, message: discord.message, data: json, call: callable, name: str=None):
+        res = call(data)
+        url = data["skin"]
 
         if (res.status_code != 200):
             await bmessage(message.channel, "❌ invalid skin")
-        else :
+        else:
             name = name or self._url_to_name(url)
             filename = self._save_tmp_img(res.content)
-            await self._send_img(message, name, filename)
+            await self._send_img(message, name, filename, url)
     
     async def _upload_manager(self, message: discord.Message):
         attachs = message.attachments
@@ -65,8 +70,9 @@ class Renderer(commands.Cog, Api):
             return
 
         url = attachs[0].url
+        data = {"skin": url}
 
-        await self._send_render(message, url)
+        await self._send_render(message, data, self.render)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -74,11 +80,12 @@ class Renderer(commands.Cog, Api):
             return
         await self._upload_manager(message)
 
-    @commands.command(aliases=["r"])
-    async def skinrender(self, ctx: commands.Context, skin_id: str):
-        """Render a skin from skins.tw with an id"""
+    async def _get_skin_url(self, ctx: commands.Context, skin_id: str) -> Any:
+        """Get skin url with an id"""
+
         base_url = config.get('API', 'DATA_API')
         res = get_api(base_url, f"api/asset/{skin_id}")
+        ret = None, None
 
         if (not res):
             await bmessage(ctx, "❌ invalid id")
@@ -86,7 +93,39 @@ class Renderer(commands.Cog, Api):
             await bmessage(ctx, "❌ this asset is not a skin")
         else:
             url = f"{base_url}/{res['path']}"
-            await self._send_render(ctx, url, res["name"])
+            ret = res, url
+        return (ret)
+
+    @commands.command(aliases=["render"])
+    async def r(self, ctx: commands.Context, skin_id: str=None, eye: str=None):
+        """Render a skin from skins.tw with an id"""
+
+        if (not skin_id):
+            return
+
+        res, url = await self._get_skin_url(ctx, skin_id)
+        data = {
+            "skin": url,
+            "eye": eye or "default_eye"
+        }
+        await self._send_render(ctx, data, self.render, res['name'])
+    
+    @commands.command(aliases=["rendercolor"])
+    async def rc(self, ctx: commands.Context, skin_id: str, bcolor: str=None, fcolor: str=None, mode: str=None, eye: str=None):
+        """Render a skin with color from skins.tw with an id"""
+
+        if (not bcolor or not fcolor or not mode):
+            return
+
+        res, url = await self._get_skin_url(ctx, skin_id)
+        data = {
+            "skin": url,
+            "bcolor": bcolor,
+            "fcolor": fcolor,
+            "mode": mode,
+            "eye": eye or 'default_eye'
+        }
+        await self._send_render(ctx, data, self.renderColor, res['name'])
 
 def setup(bot: commands.Bot):
     bot.add_cog(Renderer())
